2f5c351c54d2481a28dc6bdb1467606d260914d5
Yogendra <yogendra18@navgurukul.org>
2019-08-27 08:22:19 +0530

    Updates

---
+++ precache-manifest.7f3e04b51720e43a20b04aef3f4186c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b72b63eb0fb7a2391d9426d1712738d",
+    "url": "/Tic-Tac-Toe-Game/index.html"
+  },
+  {
+    "revision": "a19ab2312d831352fbbc",
+    "url": "/Tic-Tac-Toe-Game/static/css/main.de4b0be3.chunk.css"
+  },
+  {
+    "revision": "c88cd52adbee660b438b",
+    "url": "/Tic-Tac-Toe-Game/static/js/2.61a06326.chunk.js"
+  },
+  {
+    "revision": "a19ab2312d831352fbbc",
+    "url": "/Tic-Tac-Toe-Game/static/js/main.570fb9a9.chunk.js"
+  },
+  {
+    "revision": "bf41cf8a379e9d7741e0",
+    "url": "/Tic-Tac-Toe-Game/static/js/runtime~main.cecfa59e.js"
+  }
+]);
